38717a4c2b063a810f0013100088a46cc56a3ac9
slavog <slavog@ro.ru>
2021-12-11 03:50:45 +0400

    Authorization in Telegram

--- retriever.py
+++ retriever.py
@@ -14,8 +14,15 @@ from telethon.tl.types import ChannelParticipantsSearch
 # класс для работы с сообщениями
 from telethon.tl.functions.messages import GetHistoryRequest
 
+# Считываем учетные данные
 config = configparser.ConfigParser()
-config.read('config.ini')
+config.read("configs.ini")
+
+# Присваиваем значения внутренним переменным
 api_id = config['Telegram']['api_id']
 api_hash = config['Telegram']['api_hash']
 username = config['Telegram']['username']
+
+# Авторизация
+client = TelegramClient(username, int(api_id), api_hash)
+client.start()
