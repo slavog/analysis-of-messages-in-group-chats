e406400fc8f188440c33678bc4a374073d0cebc0
slavog <slavog@ro.ru>
2021-12-11 02:33:18 +0400

    import libraries

---
+++ retriever.py
@@ -0,0 +1,21 @@
+import configparser
+import json
+
+from telethon.sync import TelegramClient
+from telethon import connection
+
+# для корректного переноса времени сообщений в json
+from datetime import date, datetime
+
+# классы для работы с каналами
+from telethon.tl.functions.channels import GetParticipantsRequest
+from telethon.tl.types import ChannelParticipantsSearch
+
+# класс для работы с сообщениями
+from telethon.tl.functions.messages import GetHistoryRequest
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+api_id = config['Telegram']['api_id']
+api_hash = config['Telegram']['api_hash']
+username = config['Telegram']['username']
