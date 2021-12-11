b518a6baa63a0429ba33dec5ee12efd2bb695045
slavog <slavog@ro.ru>
2021-12-11 04:01:21 +0400

    change language of comments to English

--- retriever.py
+++ retriever.py
@@ -4,25 +4,25 @@ import json
 from telethon.sync import TelegramClient
 from telethon import connection
 
-# для корректного переноса времени сообщений в json
+# for correct transfer the time of messages to json
 from datetime import date, datetime
 
-# классы для работы с каналами
+# classes for working with channels
 from telethon.tl.functions.channels import GetParticipantsRequest
 from telethon.tl.types import ChannelParticipantsSearch
 
-# класс для работы с сообщениями
+# class for working with messages
 from telethon.tl.functions.messages import GetHistoryRequest
 
-# Считываем учетные данные
+# reading user data
 config = configparser.ConfigParser()
 config.read("configs.ini")
 
-# Присваиваем значения внутренним переменным
+# assign a value to a variable
 api_id = config['Telegram']['api_id']
 api_hash = config['Telegram']['api_hash']
 username = config['Telegram']['username']
 
-# Авторизация
+# Telegram authorization
 client = TelegramClient(username, int(api_id), api_hash)
 client.start()
